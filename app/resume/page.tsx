df694240b21745262855c5365f44c509aa47890f
Ephraim Becker <emb16@outlook.com>
2024-05-07 15:26:39 -0400

    Updated resume.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,50 +1,49 @@
 import Image from "next/image";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
 
 export default function Resume() {
   return (
-    <Box sx={{ p: 2, textAlign: "center" }}>
-      <Image
-        src="/ephraim-becker.jpg"
-        alt="Image of Ephraim Becker"
-        width={200}
-        height={200}
-      />
-      <Typography variant="h3" component="h3">
-        Ephraim Becker
-      </Typography>
-      <Stack spacing={2} direction="row" sx={{ justifyContent: "center" }}>
-        <Typography variant="body2" component="p">
-          emb16@outlook.com
+    <Grid container sx={{ textAlign: "center" }}>
+      <Grid item xs={12}>
+        <Image
+          src="/ephraim-becker.jpg"
+          alt="Image of Ephraim Becker"
+          width={200}
+          height={200}
+        />
+        <Typography variant="h3" component="h3">
+          Ephraim Becker
         </Typography>
+      </Grid>
+      <Grid item xs={12}>
         <Typography variant="body2" component="p">
-          |
+          emb16@outlook.com | (516) 880-3474 | Cedarhurst, NY 11516
         </Typography>
-        <Typography variant="body2" component="p">
-          (516) 880-3474
-        </Typography>
-        <Typography variant="body2" component="p">
-          |
-        </Typography>
-        <Typography variant="body2" component="p">
-          Cedarhurst, NY 11516
-        </Typography>
-      </Stack>
+      </Grid>
       <br />
       <br />
-      <Stack spacing={5} direction="row" sx={{ justifyContent: "center" }}>
+      <Grid item xs={12}>
         <Typography variant="h6" component="h4">
           Professional Summary
         </Typography>
-        <Typography variant="body2" component="p" sx={{ width: 500 }}>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body2" component="p">
           Experienced developer with a demonstrated history of working in the
           financial services industry. Skilled in HTML, CSS, JavaScript, React,
-          NextJS, Python, and Java. Strong engineering professional graduated
+          NextJS, Python, Java, and Blender. Strong engineering professional graduated
           from Ivdu.
         </Typography>
-      </Stack>
-    </Box>
+      </Grid>
+      <br />
+      <br />
+      <Grid item xs={12}>
+        <Typography variant="h6" component="h4">
+          Work History
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
