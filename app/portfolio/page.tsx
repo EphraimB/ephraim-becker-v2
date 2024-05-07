87ec52e56ef5c6e17ae117c215449423dfc997a3
Ephraim Becker <emb16@outlook.com>
2024-05-07 12:35:52 -0400

    Updated the portfolio.

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -13,7 +13,52 @@ export default function Portfolio() {
         Portfolio
       </Typography>
       <Grid container spacing={2} sx={{ m: 2 }}>
-        <Grid item xs={3}>
+        <Grid item xs={12} lg={3}>
+          <Paper sx={{ p: 2 }}>
+            <Image
+              src="/virtual-friend-screenshot.png"
+              alt="Virtual friend app screenshot"
+              width={300}
+              height={200}
+            />
+            <Typography
+              variant="body2"
+              component="p"
+              sx={{ textAlign: "center" }}
+            >
+              Started work on it {dayjs("2015-11-10").format("MMMM DD, YYYY")}{" "}
+              but gave up {dayjs("2021-08-27").format("MMMM DD, YYYY")} when I
+              converted it to Python without thinking. I then abandoned it.
+            </Typography>
+            <Typography variant="h3" component="h3">
+              Virtual Friend app
+            </Typography>
+            <Typography variant="body1" component="h4">
+              A web app that you can interact with a virtual friend. This was
+              before ChatGPT came out.
+            </Typography>
+            <Typography
+              variant="body2"
+              component="p"
+              sx={{ textAlign: "center" }}
+            >
+              <Link
+                href="https://github.com/EphraimB/Virtual-Friend"
+                target="_blank"
+              >
+                Click to download the prototype off GitHub
+              </Link>
+              <br />
+              <Link
+                href="https://ephraimb.github.io/Virtual-Friend/"
+                target="_blank"
+              >
+                You can also view it live here
+              </Link>
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} lg={3}>
           <Paper sx={{ p: 2 }}>
             <Image
               src="/budgeting-screenshot.png"
