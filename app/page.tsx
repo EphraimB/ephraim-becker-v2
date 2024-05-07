fa029ae3e0ae92b925474d274de0b24b52ed78fe
Ephraim Becker <emb16@outlook.com>
2024-05-07 16:39:17 -0400

    Added social media links to the profile card.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import styles from "./page.module.css";
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   const birthdate = "1996-07-19";
@@ -57,6 +58,87 @@ export default function Home() {
             <span style={{ fontWeight: "bold" }}>Location: </span>
             Cedarhurst, NY, United States
           </Typography>
+          <br />
+          <Stack spacing={2} direction="row">
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://www.facebook.com/ephraim.becker/"
+            >
+              <Image
+                src="/Facebook_Logo_Primary.png"
+                alt="Facebook logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://www.instagram.com/ephraim.becker/"
+            >
+              <Image
+                src="/Instagram_Glyph_Gradient.png"
+                alt="Instagram logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://twitter.com/emb180"
+            >
+              <Image
+                src="/logo-black.png"
+                alt="X logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://www.youtube.com/channel/UCIHxAXYLxYlNaQiv0do0bUg"
+            >
+              <Image
+                src="/yt_logo_rgb_light.png"
+                alt="YouTube logo"
+                width={100}
+                height={50}
+              />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://www.linkedin.com/in/ephraim-becker-3263b810b/"
+            >
+              <Image
+                src="/LI-In-Bug.png"
+                alt="Linkedin logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+            <Link
+              target="_blank"
+              rel="noopener"
+              style={{ display: "inline", backgroundColor: "rgba(0,0,0,0)" }}
+              href="https://github.com/EphraimB"
+            >
+              <Image
+                src="/github-mark.png"
+                alt="GitHub logo"
+                width={50}
+                height={50}
+              />
+            </Link>
+          </Stack>
         </Paper>
       </Container>
     </main>
