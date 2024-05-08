8c12cdc271fd8584da48b81668f57891ec1ecba4
Ephraim Becker <emb16@outlook.com>
2024-05-08 12:56:02 -0400

    Fixed error.

--- components/PDFResumeDownload.tsx
+++ components/PDFResumeDownload.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import Box from "@mui/material/Box";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import dynamic from "next/dynamic";
+const PDFDownloadLink = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  }
+);
 import ResumeDocument from "@/components/ResumeDocument";
 
 export default function PDFResumeDownload() {
