fd0572bb4101193b4af7cb13a5f45a454bb9ca83
Ephraim Becker <emb16@outlook.com>
2024-05-08 13:32:49 -0400

    Trying to render the image in the pdf.

--- components/ResumeDocument.tsx
+++ components/ResumeDocument.tsx
@@ -1,12 +1,23 @@
 "use client";
 
-import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+import {
+  Document,
+  Page,
+  Image,
+  Text,
+  View,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
     flexDirection: "row",
     backgroundColor: "#E4E4E4",
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
   },
   section: {
     margin: 10,
@@ -21,7 +32,15 @@ export default function ResumeDocument() {
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
-          <Text>Section #1</Text>
+          <Image
+            src={{
+              uri: "/ephraim-becker.jpg",
+              method: "GET",
+              headers: { "Cache-Control": "no-cache" },
+              body: "",
+            }}
+            style={styles.profileImage}
+          />
         </View>
         <View style={styles.section}>
           <Text>Section #2</Text>
