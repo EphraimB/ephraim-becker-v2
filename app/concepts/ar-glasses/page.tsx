34a7fa5105ac9345262fa7dc41b477991f008c30
Ephraim Becker <emb16@outlook.com>
2024-08-23 09:13:56 -0400

    Added concepts with ar glasses.

---
+++ app/concepts/ar-glasses/page.tsx
@@ -0,0 +1,20 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
+
+export default function ARGlasses() {
+  return (
+    <Box>
+      <Typography variant="h2" component="h2" sx={{ textAlign: "center" }}>
+        Concepts - AR glasses
+      </Typography>
+    </Box>
+  );
+}
--- app/concepts/page.tsx
+++ app/concepts/page.tsx
@@ -1,10 +1,14 @@
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Image from "next/image";
 import Link from "next/link";
-import dayjs from "dayjs";
 import Box from "@mui/material/Box";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
 
 export default function Concepts() {
   return (
@@ -12,6 +16,26 @@ export default function Concepts() {
       <Typography variant="h2" component="h2" sx={{ textAlign: "center" }}>
         Concepts
       </Typography>
+      <Grid container spacing={2} sx={{ p: 2 }}>
+        <Grid item xs={12} sx={{ textAlign: "center" }}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardHeader title="AR glasses" />
+            <CardContent>
+              <Typography component="p" variant="subtitle1">
+                Glasses to see the experience
+              </Typography>
+              <Typography component="p" variant="subtitle2">
+                Launching in the far future
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Link href="concepts/ar-glasses" as="concepts/ar-glasses">
+                <Button size="small">Learn More</Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
