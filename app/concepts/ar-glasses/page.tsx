9c79875385e21bca2728f841a7dc22c05d3c3e6d
Ephraim Becker <emb16@outlook.com>
2024-08-25 08:30:22 -0400

    Added concept images.

--- app/concepts/ar-glasses/page.tsx
+++ app/concepts/ar-glasses/page.tsx
@@ -1,13 +1,7 @@
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-} from "@mui/material";
+import Image from "next/image";
+import { Stack } from "@mui/material";
 
 export default function ARGlasses() {
   return (
@@ -15,6 +9,32 @@ export default function ARGlasses() {
       <Typography variant="h2" component="h2" sx={{ textAlign: "center" }}>
         Concepts - AR glasses
       </Typography>
+      <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+        <Image
+          width="200"
+          height="100"
+          src="/ar-glasses-concept/AR-glasses-concept.jpg"
+          alt="Actual reality"
+        />
+        <Image
+          width="200"
+          height="100"
+          src="/ar-glasses-concept/AR-glasses-concept-2.jpg"
+          alt="Augmented reality with just HUD"
+        />
+        <Image
+          width="200"
+          height="100"
+          src="/ar-glasses-concept/AR-glasses-concept-3.jpg"
+          alt="Augmented reality with HUD and watch app"
+        />
+        <Image
+          width="200"
+          height="100"
+          src="/ar-glasses-concept/AR-glasses-concept-4.jpg"
+          alt="Augmented reality with HUD and maps app"
+        />
+      </Stack>
     </Box>
   );
 }
