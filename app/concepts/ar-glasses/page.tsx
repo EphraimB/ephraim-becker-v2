d087f5d41914aa9e546b1e916e6f3725e0c416bb
Ephraim Becker <emb16@outlook.com>
2024-08-25 08:37:16 -0400

    Added framer motion.

--- app/concepts/ar-glasses/page.tsx
+++ app/concepts/ar-glasses/page.tsx
@@ -2,35 +2,39 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Image from "next/image";
 import { Stack } from "@mui/material";
+import { motion } from "framer-motion";
 
 export default function ARGlasses() {
+  const width = 500;
+  const height = 200;
+
   return (
     <Box>
       <Typography variant="h2" component="h2" sx={{ textAlign: "center" }}>
         Concepts - AR glasses
       </Typography>
-      <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+      <Stack direction="column" spacing={2} sx={{ alignItems: "center" }}>
         <Image
-          width="200"
-          height="100"
+          width={width}
+          height={height}
           src="/ar-glasses-concept/AR-glasses-concept.jpg"
           alt="Actual reality"
         />
         <Image
-          width="200"
-          height="100"
+          width={width}
+          height={height}
           src="/ar-glasses-concept/AR-glasses-concept-2.jpg"
           alt="Augmented reality with just HUD"
         />
         <Image
-          width="200"
-          height="100"
+          width={width}
+          height={height}
           src="/ar-glasses-concept/AR-glasses-concept-3.jpg"
           alt="Augmented reality with HUD and watch app"
         />
         <Image
-          width="200"
-          height="100"
+          width={width}
+          height={height}
           src="/ar-glasses-concept/AR-glasses-concept-4.jpg"
           alt="Augmented reality with HUD and maps app"
         />
