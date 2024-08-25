011067f356e378f61dfa9cb9e24ed37cfe57e0f1
Ephraim Becker <emb16@outlook.com>
2024-08-25 08:45:32 -0400

    Seperated concept scenes.

--- app/concepts/ar-glasses/page.tsx
+++ app/concepts/ar-glasses/page.tsx
@@ -1,7 +1,6 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Image from "next/image";
-import { Stack } from "@mui/material";
 import { motion } from "framer-motion";
 
 export default function ARGlasses() {
@@ -13,32 +12,57 @@ export default function ARGlasses() {
       <Typography variant="h2" component="h2" sx={{ textAlign: "center" }}>
         Concepts - AR glasses
       </Typography>
-      <Stack direction="column" spacing={2} sx={{ alignItems: "center" }}>
+      <motion.div>
         <Image
           width={width}
           height={height}
           src="/ar-glasses-concept/AR-glasses-concept.jpg"
           alt="Actual reality"
         />
+        <Typography component="p" variant="subtitle1">
+          This is the actual environment before you put on your glasses.
+        </Typography>
+      </motion.div>
+      <motion.div>
         <Image
           width={width}
           height={height}
           src="/ar-glasses-concept/AR-glasses-concept-2.jpg"
           alt="Augmented reality with just HUD"
         />
+        <Typography component="p" variant="subtitle1">
+          When you put on your glasses and swipe up, you get the HUD.
+        </Typography>
+      </motion.div>
+      <motion.div>
         <Image
           width={width}
           height={height}
           src="/ar-glasses-concept/AR-glasses-concept-3.jpg"
           alt="Augmented reality with HUD and watch app"
         />
+        <Typography component="p" variant="subtitle1">
+          When you tap on the watch app, it puts a virtual watch on your wrist
+          and you can even choose different watches and even watchfaces.
+        </Typography>
+      </motion.div>
+      <motion.div>
         <Image
           width={width}
           height={height}
           src="/ar-glasses-concept/AR-glasses-concept-4.jpg"
           alt="Augmented reality with HUD and maps app"
         />
-      </Stack>
+        <Typography component="p" variant="subtitle1">
+          You can even go to the maps app and view maps placed on your own
+          table.
+        </Typography>
+      </motion.div>
+      <motion.div>
+        <Typography component="p" variant="subtitle1">
+          And that's only the beginning. More to come..
+        </Typography>
+      </motion.div>
     </Box>
   );
 }
